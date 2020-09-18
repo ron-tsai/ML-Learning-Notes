1a24bf327caf8950a312bfc4b2c38184c85da2fc
ron <759782075@qq.com>
2020-09-19 00:06:33 +0800

    #pandas数学建模处理

---
+++ he_bing_excel.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+files=[file for file in os.listdir('C:\\Users\Administrator\Desktop\data\S1\\train_data_label')]
+print(files)
+all_goods=pd.DataFrame()
+for file in files:
+    df=pd.read_excel('C:\\Users\Administrator\Desktop\data\S1\\train_data_label\\'+file)
+    all_goods=pd.concat([all_goods,df])
+# print(all_goods)
+all_goods.to_excel('C:\\Users\Administrator\Desktop\data\S1\\combine_1_train_data.xlsx')
